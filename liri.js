c129d2facf31518c991edf4ed71a1507695d2bcb
Kristen De La Rosa <delarosa.kristen@gmail.com>
2019-04-27 23:52:31 -0400

    Spotify API added

--- liri.js
+++ liri.js
@@ -7,18 +7,19 @@ var axios = require("axios");
 var moment = require("moment");
 var Spotify = require('node-spotify-api');
 
-var choice = "",
+// var choice = "",
 
 inquirer.prompt([{
   type: 'list',
   name: 'listChoices',
-  message: "What do you want to do?",
+  message: 'What do you want to do?',
   choices: ['Look up Concerts?', 'Search for a Song?', 'Find a Movie?']
 }
 ]).then(function(response){
 
   // var choicelist = response.command;
   // choice =
+
   switch (response.listChoices) {
     case 'Look up Concerts?':
      bandsApi();
@@ -37,17 +38,45 @@ function bandsApi(){
   inquirer.prompt([{
     type:'input',
     name: 'band'
-    message:"Which band would you like to search?",
+    message: "Which band would you like to search?"
   }
   ]).then(function(response){
     var searchBand = response.band;
 
-    axios.get("https://rest.bandsintown.com/artists/" + searchTerm + "/events?app_id=codingbootcamp")
+    axios.get("https://rest.bandsintown.com/artists/" + searchBand + "/events?app_id=codingbootcamp")
     .then(function(response){
       for(var i =0; i < response.data.length; i++){
-        
+        var bResult = response.data[1].datetime;
+        console.log("Date of the Event: " + moment(bResult).format("MM/DD/YYYY"));
+        console.log("Name of the Venue: " + response.data[i].venue.name);
+        console.log("Location: " + response.data[i].venue.city);
 
       }
     })
   })
 }
+
+//Spotify API
+function spotifyApi(){
+  inquirer.prompt([{
+    type:'input',
+    name: 'spotify'
+    message: "Which song would you like to search?"
+  }
+  ]).then(function(response){
+    var searchSpot = response.spotify;
+
+    axios.get("https://rest.bandsintown.com/artists/" + searchSpot + "/events?app_id=codingbootcamp")
+    .then(function(response){
+      for(var i =0; i < response.spot.length; i++){
+        // var sResult = response.spot[1];
+        console.log("Artist(s): " + response.spot[i].artist);
+        console.log("Song: " + response.spot[i].items);
+        console.log("Album: " + response.data[i].album);
+
+      }
+    })
+  })
+}
+
+;
